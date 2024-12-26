44489ce21e0458ad9f88e31b3e2421d94fa0b183
maheshbasnet089 <basnetmanish089@gmail.com>
2024-12-26 22:08:26 +0545

    first commit

---
+++ src/store/store.ts
@@ -0,0 +1,13 @@
+import { configureStore } from "@reduxjs/toolkit";
+import productSlice from "./productSlice";
+import userSlice from "./userSlice";
+
+
+const store = configureStore({
+    reducer : {
+        haha : productSlice, 
+        hehe : userSlice, 
+    }
+})
+
+export default store 
---
+++ src/store/userSlice.ts
@@ -0,0 +1,26 @@
+import { createSlice,PayloadAction } from "@reduxjs/toolkit";
+import { User } from "./types";
+
+
+
+const userInfo:User = {
+    name : "manish basnet", 
+    age : 22
+}
+
+const userSlice = createSlice({
+    name : "user", 
+    initialState : userInfo, 
+    reducers : {
+        setName(state:User,action:PayloadAction<User>){
+            state.name = "hahahehehuhu"
+        }, 
+        setAge(state:User,action:PayloadAction<User>){
+            state.age = 12
+        }
+    }
+})
+// actions 
+export const {setName,setAge} = userSlice.actions
+export default userSlice.reducer
+
