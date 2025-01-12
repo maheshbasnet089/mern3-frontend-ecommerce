1f3b57abe6407d5eb9118dcafc1f6835e7e7a8ad
maheshbasnet089 <basnetmanish089@gmail.com>
2025-01-12 21:32:07 +0545

    fetch product

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import { Provider } from 'react-redux'
 import store from './store/store'
 import Home from './pages /home/Home'
 import Login from './pages /user/Login'
+import Product from './pages /product/Product'
 
 function App() {
 
@@ -16,6 +17,7 @@ function App() {
     <Route path='/' element={<Home />} />
     <Route path='/register' element={<Register />}/>
     <Route path='/login' element={<Login />}/>
+    <Route path='/products' element={<Product />}/>
     
     </Routes>
     </BrowserRouter>
