e0daf187324005e1d7a0e519fbe0bcedc2d76347
maheshbasnet089 <basnetmanish089@gmail.com>
2025-01-12 21:33:00 +0545

    add category

--- src/pages /product/components/Card.tsx
+++ src/pages /product/components/Card.tsx
@@ -10,7 +10,7 @@ const Card:React.FC<ICardProps> = ({product})=>{
         <a href="#">
           <img src="https://images.unsplash.com/photo-1646753522408-077ef9839300?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwcm9maWxlLXBhZ2V8NjZ8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60" alt="Product" className="h-80 w-72 object-cover rounded-t-xl" />
           <div className="px-4 py-3 w-72">
-            <span className="text-gray-400 mr-3 uppercase text-xs">Brand</span>
+            <span className="text-gray-400 mr-3 uppercase text-xs">{product.Category.categoryName}</span>
             <p className="text-lg font-bold text-black truncate block capitalize">{product.productName}</p>
             <div className="flex items-center">
               <p className="text-lg font-semibold text-black cursor-auto my-3">$ {product.productPrice}</p>
