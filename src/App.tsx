b93d90f836599f923b4071e1d92c7ca9d48d6367
maheshbasnet089 <basnetmanish089@gmail.com>
2025-01-13 21:22:11 +0545

    add single

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ import store from './store/store'
 import Home from './pages /home/Home'
 import Login from './pages /user/Login'
 import Product from './pages /product/Product'
+import SingleProduct from './pages /single-product/SingleProduct'
 
 function App() {
 
@@ -18,6 +19,7 @@ function App() {
     <Route path='/register' element={<Register />}/>
     <Route path='/login' element={<Login />}/>
     <Route path='/products' element={<Product />}/>
+    <Route path='/products/:id' element={<SingleProduct />}/>
     
     </Routes>
     </BrowserRouter>
